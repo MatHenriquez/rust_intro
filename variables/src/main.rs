54b6fd74080683a264f3a7a04b2d8e741832984b
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-02-26 17:32:21 -0300

    Add variable type comments and constants

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     let first_number = 10;
     println!("The value of first_number is: {}", first_number);
 
-    let second_number: i32 = 20;
+    let second_number: i32 = 20; // Is optional to specify the type of the variable
     let sum = first_number + second_number;
 
     println!("The value of sum is: {}", sum);
@@ -16,4 +16,10 @@ fn main() {
 
     mutable_number = 40;
     println!("The value of mutable_number is: {}", mutable_number);
+
+    // Constants
+    const PI: f32 = 3.1416; // Is mandatory to specify the type of the constant
+    static LANGUAGE: &str = "Rust";
+    println!("The value of PI is: {}", PI);
+    println!("The value of LANGUAGE is: {}", LANGUAGE);
 }
