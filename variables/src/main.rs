048de077ad1eb1b0094c9bd51751544de5033ed8
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-02-26 17:23:32 -0300

    Update second_number variable type to i32

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let first_number = 10;
     println!("The value of first_number is: {}", first_number);
 
-    let second_number = 20;
+    let second_number: i32 = 20;
     let sum = first_number + second_number;
 
     println!("The value of sum is: {}", sum);
