c923d65f4a2ae0ff251df4f6d09ecc82e8e68f14
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-02-26 17:37:46 -0300

    Add variable shadowing examples

--- shadowing/src/main.rs
+++ shadowing/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let value: i32 = 10; // Is inmutable by default
+    println!("The value of value is: {}", value);
+
+    let value: i32 = 20; // Shadowing the previous value
+    println!("The new value of value is: {}", value);
+
+    let value = false; // Shadowing the previous value with a different type
 }
