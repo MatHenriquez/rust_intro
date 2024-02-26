8dbac602f3d1cb522553e3733c7eec7d368c565c
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-02-26 17:14:38 -0300

    Add sum example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
     let first_number = 10;
     println!("The value of first_number is: {}", first_number);
+
+    let second_number = 20;
+    let sum = first_number + second_number;
+
+    println!("The value of sum is: {}", sum);
 }
