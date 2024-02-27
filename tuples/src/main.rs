a379385bc90c68ff9ce5f14482537529003fcd74
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-02-27 09:52:56 -0300

    Add tuple definitions, access and destructuring

--- tuples/src/main.rs
+++ tuples/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let tuple = (1, false, 5.5); // Tuple can store different types
+    println!("Tuple: {:?}", tuple);
+
+    let mut tuple: (i32, bool, f64) = (1, false, 5.5);
+    println!("Tuple: {:?}", tuple);
+
+    let (x, y, z) = tuple; // Destructuring
+    println!("x: {}, y: {}, z: {}", x, y, z);
+
+    let first_element = tuple.0; // Accessing elements by index
+    let last_element = tuple.2;
+    println!("First element: {}, Last element: {}", first_element, last_element);
+
+    tupla.1 = true; // Updating tuple element
+    println!("Tuple: {:?}", tuple);
 }
