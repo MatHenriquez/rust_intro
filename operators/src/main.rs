89487b91986508990e06ac72a98a742a79bf7d64
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-02-27 09:34:47 -0300

    Exemplify logic and comparison operators

--- operators/src/main.rs
+++ operators/src/main.rs
@@ -12,8 +12,17 @@ fn main() {
     let is_major = number_one > number_two;
     let is_minor = number_one < number_two;
     let is_equal = number_one == number_two;
+    let is_major_or_equal = number_one >= number_two;
+    let is_minor_or_equal = number_one <= number_two;
+    let is_different = number_one != number_two;
+    let result: bool = 20 + 10 > 100 || true;
+    let result2: bool = 20 + 10 > 100 && true;
 
     println!("Sum: {}, Sub: {}, Mul: {}, Div: {}, Rem: {}", sum, sub, mul, div, rem);
     println!("Pow: {}, Sqrt: {}", pow, sqrt);
     println!("Is major: {}, Is minor: {}, Is equal: {}", is_major, is_minor, is_equal);
+    println!("Is major or equal: {}, Is minor or equal: {}", is_major_or_equal, is_minor_or_equal);
+    println!("Is different: {}", is_different);
+    println!("Result: {}", result);
+    println!("Result2: {}", result2);
 }
