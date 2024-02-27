270bb3364cdfa14f3c5df46563321ef173f2f2a9
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-02-27 09:41:03 -0300

    Add array definitions

--- arrays/src/main.rs
+++ arrays/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let numbers = [1, 2, 3, 4, 5];
+    println!("First number: {}", numbers[0]);
+    println!("Numbers: {:?}", numbers);
+
+    let numbers: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("Numbers: {:?}", numbers);
+
+    let values = [5.5; 10]; // [5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5,]
+    println!("Values: {:?}", values);
 }
