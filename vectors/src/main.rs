c136d897aec9a08e745f12f3c2dccf2f07f32edb
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-02-29 16:58:25 -0300

    Exemplify vector definition and methods

--- vectors/src/main.rs
+++ vectors/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let vector = vec![1, 2, 3, 4, 5]; // Vector can store same type and change size
+    println!("Vector: {:?}", vector);
+
+    let mut vector = vec![1, 2, 3, 4, 5];
+    println!("Vector: {:?}", vector);
+
+    let first_element = vector[0]; // Accessing elements by index
+    let last_element = vector[vector.len() - 1];
+    println!("First element: {}, Last element: {}", first_element, last_element);
+
+    vector[1] = 10; // Updating vector element
+    println!("Vector: {:?}", vector);
+
+    vector.push(6); // Adding element to the end
+    println!("Vector: {:?}", vector);
+
+    let last_element = vector.pop(); // Removing element from the end
+    println!("Vector: {:?}, Last element: {:?}", vector, last_element);
+
+    let first_element = vector.remove(0); // Removing element by index
+    println!("Vector: {:?}, First element: {}", vector, first_element);
 }
