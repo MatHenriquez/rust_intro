d02318205e6cafa2c480a0ec76e01cac96139bb1
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-01 09:30:03 -0300

    Exemplify code blocks and variable scopes

--- code_blocks/src/main.rs
+++ code_blocks/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
-}
+    let message_main = "Hello, world from main() block!";
+    println!("{}", message_main);
+
+    {
+        let message_inner = "Hello, world from inner block!"; // Scope: message_inner is accessible only within the block
+        println!("{}", message_inner);
+        println!("{} (from inner block)", message_main); // Scope: message_main is accessible from inner block but not vice versa
+    }
+} // When the block ends, the variable is destroyed
+
+// Note: shadowing respect the scope of the variable. It does not affect the variable outside the block.
