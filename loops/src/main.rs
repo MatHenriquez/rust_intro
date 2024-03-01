e683cbf023e23d7a162b4362d720fe6e526bd044
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-01 17:40:35 -0300

    Add loop example

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+
+    let mut count = 0;
+
+    loop {
+        count += 1;
+        println!("{}", count);
+
+        if count == 5 {
+            break;
+        }
+    }
 }
