d8c78eee59dcf287b7140b8352913e6a8d1eeca9
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-06 09:37:22 -0300

    Exemplify match with returned value

--- matchs/src/main.rs
+++ matchs/src/main.rs
@@ -12,4 +12,15 @@ fn main() {
         },
         _ => println!("It's something else"),
     }
+
+    let message = match number {
+        1 => "It's one",
+        2 => "It's two",
+        3 => "It's three",
+        4 | 5 => "It's four or five",
+        6..=100 => "It's greater than five and less than or equal to one hundred",
+        _ => "It's something else",
+    };
+
+    println!("Message is: {:?}", message);
 }
