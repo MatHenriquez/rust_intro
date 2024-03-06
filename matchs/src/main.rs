26e667f843425571afdc3795d6da300b0875bbed
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-06 12:09:06 -0300

    Add comments

--- matchs/src/main.rs
+++ matchs/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
         1 => println!("It's one"),
         2 => println!("It's two"),
         3 => println!("It's three"),
-        4 | 5 => println!("It's four or five"),
+        4 | 5 => println!("It's four or five"), // Match multiple patterns
         6..=100 => {
             println!("It's greater than five");
             println!("It's less than or equal to one hundred");
