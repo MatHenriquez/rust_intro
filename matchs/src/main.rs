3312458cef4b5df6f54cf04aa82570e0f969fadf
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-06 09:34:00 -0300

    Exemplify more complex cases

--- matchs/src/main.rs
+++ matchs/src/main.rs
@@ -1,10 +1,15 @@
 fn main() {
-    let number: i32 = 3;
+    let number: i32 = 50;
 
     match number {
         1 => println!("It's one"),
         2 => println!("It's two"),
         3 => println!("It's three"),
+        4 | 5 => println!("It's four or five"),
+        6..=100 => {
+            println!("It's greater than five");
+            println!("It's less than or equal to one hundred");
+        },
         _ => println!("It's something else"),
     }
 }
