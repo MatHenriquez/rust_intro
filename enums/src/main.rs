62bf3ce11360cbd1c9411df0a33d0caa2baad918
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-06 09:52:33 -0300

    Exemplify enum declaration

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    enum Response {
+        Success,
+        Error(u32), // Error with a code (u32) associated with it (like HTTP status code)
+    }
+
+    let response = Response::Success; // Success response
+    let response = Response::Error(404); // Error response with code 404
+
+    match response {
+        Response::Success => println!("Request succeeded"), // Print if response is Success
+        Response::Error(404) => println!("Request failed: Not Found"), // Print if response is Error with code 404
+        Response::Error(code) => println!("Request failed with code: {}", code), // Print if response is Error with any other code
+    }
 }
