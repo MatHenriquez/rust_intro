60f16fb033cb9eeaaa45b79bc3682a9fb3aea86a
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-06 09:32:09 -0300

    Exemplify match statement

--- matchs/src/main.rs
+++ matchs/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let number: i32 = 3;
+
+    match number {
+        1 => println!("It's one"),
+        2 => println!("It's two"),
+        3 => println!("It's three"),
+        _ => println!("It's something else"),
+    }
 }
