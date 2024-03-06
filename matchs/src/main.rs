d4190bb0400e7fc7cdc7cf10c736a78a56120877
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-06 09:43:40 -0300

    Resolve FizzBuzz exercise using match

--- matchs/src/main.rs
+++ matchs/src/main.rs
@@ -23,4 +23,14 @@ fn main() {
     };
 
     println!("Message is: {:?}", message);
+
+    // FizzBuzz
+    for number in 1..=100 {
+        match (number % 3, number % 5) {
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            _ => println!("{}", number),
+        }
+    }
 }
