2927f64e353b52e4febf0df55490222b8485391e
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-11 09:19:39 -0300

    Exemplify function declarations

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,14 @@
+fn user_greeting(){
+    println!("Hello, user!");
+}
+
+fn sum(number_one: i32, number_two: i32) -> i32 {
+    number_one + number_two
+}
+
 fn main() {
-    println!("Hello, world!");
+    user_greeting();
+
+    let result = sum(10, 20);
+    println!("The result is: {}", result);
 }
