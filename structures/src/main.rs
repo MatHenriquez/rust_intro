0ed2603ee1dfb4c47684ef982a0d814aadc070a7
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-14 16:48:37 -0300

    Create User struct

--- structures/src/main.rs
+++ structures/src/main.rs
@@ -1,3 +1,13 @@
+struct User { // struct definition 
+    username: String, // struct fields
+    password: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user = User { // struct instance
+        username: String::from("user"),
+        password: String::from("password"),
+    };
+    println!("Username: {}", user.username);
+    println!("Password: {}", user.password);
 }
