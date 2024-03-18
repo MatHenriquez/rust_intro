3320a95d60e9347aaff59466bf219300beeb3405
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-18 09:35:04 -0300

    Add tuple struct examples

--- tuple_structs/src/main.rs
+++ tuple_structs/src/main.rs
@@ -1,3 +1,10 @@
+#[derive(Debug)] // to print the struct with {:?}
+struct Color (u32, u32, u32); // tuple struct with 3 elements 
+
 fn main() {
-    println!("Hello, world!");
+    let black = Color(0, 0, 0);
+    let white = Color(255, 255, 255);
+
+    println!("black: {:?}", black);
+    println!("white: {:?}", white);
 }
