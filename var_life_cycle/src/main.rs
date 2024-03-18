4881bc2ff45b43bf282337aa3d8d69d4a58f5d90
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-18 09:51:02 -0300

    Exemplify variable scope and life cycle

--- var_life_cycle/src/main.rs
+++ var_life_cycle/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let message = String::from("Hello, world! From main!");
+
+    { // Block 2: new scope
+        println!("message from block 2: {}", message);
+        let message = String::from("Hello, world! From block 2!");
+        { // Block 3: new scope
+            println!("message from block 3: {}", message); // This will print the message from block 2 because it's the closest scope
+            let result = message.len() + 10; // This variable will be dropped at the end of this block
+        }
+    }
 }
