2f57a4d5f1e92820eb9972cda56dab61cd34285f
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-18 09:39:51 -0300

    Add print statement to display message

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -2,4 +2,7 @@ fn main() {
     // A slice is a reference to a contiguous sequence of elements in a collection. Are like references to arrays, but they don't have a fixed size.
     // Slices -> Heap-allocated data structures
     // Arrays -> Stack-allocated data structures
+
+    let message = String::from("Hello, world!");
+    println!("message: {}", message);
 }
