b209257905453b82a9e142cc2e440a0af3f650d4
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-18 09:56:35 -0300

    Exemplify borrowing

--- var_life_cycle/src/main.rs
+++ var_life_cycle/src/main.rs
@@ -9,4 +9,12 @@ fn main() {
             let result = message.len() + 10; // This variable will be dropped at the end of this block
         }
     }
+
+    let new_message = String::from("Hello, world! From main! Again!"); // This is a new variable, not the same as the first one
+
+    {
+        let borrowed_message = &new_message; // This is a reference to the message variable
+    }
+
+    println!("message from main: {}", new_message); // This will print the message from the main scope
 }
