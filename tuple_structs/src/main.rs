619e79fd38bba71e8dc13c0036becf78171f8cea
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-18 09:37:20 -0300

    Add custom_color and update first element value

--- tuple_structs/src/main.rs
+++ tuple_structs/src/main.rs
@@ -7,4 +7,10 @@ fn main() {
 
     println!("black: {:?}", black);
     println!("white: {:?}", white);
+
+    let mut custom_color = Color(100, 200, 150);
+    println!("custom_color: {:?}", custom_color);
+
+    // change the value of the first element
+    custom_color.0 = custom_color.0 + 10;   
 }
