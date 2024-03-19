2128ffb4ad9bcc05fb0024289dae7c6b115829bb
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-19 17:27:33 -0300

    Add Option enum comments

--- options/src/main.rs
+++ options/src/main.rs
@@ -1,3 +1,8 @@
+// enum Option<T> { // Option -> is an enum
+//     Some(T), // Some -> is a variant of Option
+//     None, // None -> is a variant of Option
+// }
+
 fn main() {
     // Option -> is an enum that can have two values: Some and None
     // Result -> is an enum that can have two values: Ok and Err
