8d5eecc75084c61fa872ad0b63a240bd5d71cbb6
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-19 17:25:02 -0300

    Add panic message example to main function

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! - line 2");
+    println!("Hello, world! - line 3");
+    println!("Hello, world! - line 4");
+    panic!("Crash and burn!"); // This will cause the program to panic and print the message
+    println!("Hello, world! - line 6");
 }
