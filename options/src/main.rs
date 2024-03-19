7372293ac73a2bafbc38a7552e3bff6053811abd
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-19 17:30:16 -0300

    Update get_value function call in main.rs

--- options/src/main.rs
+++ options/src/main.rs
@@ -15,7 +15,7 @@ fn main() {
     // Option -> is an enum that can have two values: Some and None
     // Result -> is an enum that can have two values: Ok and Err
 
-    let message = get_value(); // This will return Some("Hello, world! From get_value!") because the function returns a value
+    let message = get_value(true); // This will return Some("Hello, world! From get_value!") because the function returns a value
     match message {
         Some(msg) => println!("{}", msg), // This will print the message
         None => println!("No message"), // This will not be executed
