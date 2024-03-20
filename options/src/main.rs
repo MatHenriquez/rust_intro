3a138286c2d5dc5a7d51d24cfd17cca71c15078d
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-03-20 09:21:19 -0300

    Add User struct with optional age field

--- options/src/main.rs
+++ options/src/main.rs
@@ -11,6 +11,13 @@ fn get_value(flag: bool) -> Option<String> {
     }
 }
 
+struct User {
+    username: String,
+    email: String,
+    password: String,
+    age: Option<u32>, // It's optional
+}
+
 fn main() {
     // Option -> is an enum that can have two values: Some and None
     // Result -> is an enum that can have two values: Ok and Err
